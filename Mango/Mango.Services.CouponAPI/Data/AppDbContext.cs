8b2c9011ec7f1c287a042869b4525dedeaa326c1
Lushen Moodley <lushenmoodley@yahoo.com>
2025-02-07 17:56:10 +0200

    impleent migration

---
+++ Mango/Mango.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContext) : base(dbContext) 
+        {
+        
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 3,
+                CouponCode = "AAA",
+                DiscountAmount = 10,
+                MinAmount = 20
+            });
+        }
+    }
+}
