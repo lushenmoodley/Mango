40efefe33713eaea3dc24ae7eba27e168fc1dcda
Lushen Moodley <lushenmoodley@yahoo.com>
2025-02-09 20:23:57 +0200

    correction

---
+++ Mango/Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,60 @@
+﻿using Mango.Services.CouponAPI.Data;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _response;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+
+                return objList;
+
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon objList = _db.Coupons.First(u=>u.CouponId==id);
+                _response
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return null;
+        }
+
+     
+    }
+}
---
+++ Mango/Mango.Services.CouponAPI/Migrations/20250209151743_addDB.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 3);
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[] { 1, "AAA", 10.0, 20 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[] { 3, "AAA", 10.0, 20 });
+        }
+    }
+}
