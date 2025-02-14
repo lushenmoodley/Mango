66b2e5c339301a84e1ca4f22e9e06debfc25ed44
Lushen Moodley <lushenmoodley@yahoo.com>
2025-02-14 22:14:13 +0200

    Added project web app

---
+++ Mango/Mango.Web/Models/Dto/RequestDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Mango.Web.Models.Dto
+{
+    public class RequestDto
+    {
+        public string ApiType { get; set; } = "Get";
+
+        public string Url { get; set; }
+
+        public object Data { get; set; }
+
+        public string AccessToken { get; set; }
+    }
+}
