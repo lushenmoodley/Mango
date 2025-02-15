5412de468954fa32adf35e41f89c175e38822fde
Lushen Moodley <lushenmoodley@yahoo.com>
2025-02-15 11:22:44 +0200

    coupon component implmeneted

---
+++ Mango/Mango.Web/Service/CouponService.cs
@@ -0,0 +1,73 @@
+﻿using Mango.Web.Models.Dto;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+
+        public async Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.POST,
+                Url = StaticDetails.CouponAPIBase + "/api/coupon/",
+                Data = couponDto
+            });
+        }
+
+        public async Task<ResponseDto> DeleteCouponAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.DELETE,
+                Url = StaticDetails.CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllCouponAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType=StaticDetails.ApiType.GET,
+                Url=StaticDetails.CouponAPIBase+"/api/coupon"
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.CouponAPIBase + "/api/GetByCode/"+couponCode
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.GET,
+                Url = StaticDetails.CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = StaticDetails.ApiType.PUT,
+                Url = StaticDetails.CouponAPIBase + "/api/coupon/",
+                Data = couponDto
+            });
+        }
+    }
+}
