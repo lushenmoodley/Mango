b5685b11d19f9b1322b35b3dbfd5531ada51ee21
Lushen Moodley <lushenmoodley@yahoo.com>
2025-10-14 20:15:36 +0200

    Added coupon model

--- Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango/Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cb049dca246e59db727a39e23784f9f939c542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
