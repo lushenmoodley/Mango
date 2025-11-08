9ac0774a7a23303e08d624fa2d307fceb7aaf54e
Lushen Moodley <lushenmoodley@yahoo.com>
2025-11-08 20:29:24 +0200

    Added authentication

---
+++ Mango/Mango.Services.AuthAPI/Models/Dto/RegistrationRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Mango.Services.AuthAPI.Models.Dto
+{
+    public class RegistrationRequestDto
+    {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+    }
+}
